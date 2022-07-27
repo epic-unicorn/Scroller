39514758a17c6483abafa4777ee8366dcb91f62f
epic-unicorn <struijs@gmail.com>
2022-07-27 21:20:08 +0200

    framework update and published setup 1.0.0.0

--- Scroller/Properties/Resources.Designer.cs
+++ Scroller/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Scroller.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -89,7 +89,7 @@ namespace Scroller.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Close Scroller?.
+        ///   Looks up a localized string similar to Close Application?.
         /// </summary>
         internal static string WindowClosing_Are_you_sure_to_close_this_application_ {
             get {
